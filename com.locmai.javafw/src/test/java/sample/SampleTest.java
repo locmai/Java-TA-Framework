1dcbe63878b06f9e16b014517d9dcc69a3f5af94
locmai <locmai0201@gmail.com>
2017-09-18 03:55:57 +0700

    update Capabilities setting for Chrome

--- com.locmai.javafw/src/test/java/sample/SampleTest.java
+++ com.locmai.javafw/src/test/java/sample/SampleTest.java
@@ -14,7 +14,7 @@ import factories.DriverFactory;
 
 public class SampleTest {
 	WebDriver driver = DriverFactory.getDriver(DriverType.CHROME,DeviceType.IPAD_MINI);
-	
+	//WebDriver driver = DriverFactory.getDriver(DriverType.CHROME);
 	@Test
 	public void f() {
 		driver.get("https://www.whatismybrowser.com/");
