1494c86182ef4f0a38f3ddedb48a6333f3a52f0e
locmai <locmai0201@gmail.com>
2017-09-30 17:15:16 +0700

    refactoredd data helper

--- com.locmai.javafw/src/main/java/utilities/DataHelper.java
+++ com.locmai.javafw/src/main/java/utilities/DataHelper.java
@@ -1,6 +1,6 @@
 package utilities;
 
-import java.io.File; 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +18,8 @@ import utilities.PathHelper;
 
 public final class DataHelper {
 	private Workbook currentWorkbook;
-	private Map<String,Sheet> allSheet = new HashMap<String, Sheet>();
-	
+	private Map<String, Sheet> allSheet = new HashMap<String, Sheet>();
+
 	public DataHelper(String filePath) {
 		String path = ExcelPath(filePath);
 		try {
@@ -32,44 +32,48 @@ public final class DataHelper {
 	}
 
 	private void addAllSheet() {
-		for (int i = 0; i < currentWorkbook.getNumberOfSheets(); i++)
-		{
+		for (int i = 0; i < currentWorkbook.getNumberOfSheets(); i++) {
 			Sheet tmpSheet = currentWorkbook.getSheetAt(i);
-		    allSheet.put(tmpSheet.getSheetName(), tmpSheet);
+			allSheet.put(tmpSheet.getSheetName(), tmpSheet);
 		}
 	}
 
-	
-	public List<String> getColumnData(String headerName,String sheetName) {
+	public List<String> getColumnData(String headerName, String sheetName) {
+		List<String> tmpColumnData = new ArrayList<>();
 		Sheet selectedSheet = getSheet(sheetName);
-		int index = getColumnIndex(headerName, selectedSheet);
+		int columnIndex = getColumnIndex(headerName, selectedSheet);
 		
-		List<String> cellColumnData = new ArrayList<>();
-		for (int i = 1; i<=selectedSheet.getLastRowNum();i++ ) {
-		
+		for (int rowIndex = 1; rowIndex <= selectedSheet.getLastRowNum(); rowIndex++) {
+			Row row = selectedSheet.getRow(rowIndex);
+			if (row != null) {
+				Cell cell = row.getCell(columnIndex);
+				if (cell != null) {
+					tmpColumnData.add(cell.getStringCellValue());
+				}
+			}
 		}
-		return null;
-	}
-	
-	private int getColumnIndex(String headerName,Sheet selectedSheet) {
-		Row row = selectedSheet.getRow(0);
-		int index=0;
 		
+		return tmpColumnData;
+	}
+
+	private int getColumnIndex(String headerName, Sheet selectedSheet) {
+		Row row = selectedSheet.getRow(0);
+		int index = 0;
+
 		Iterator<Cell> cellIterator = row.cellIterator();
 		while (cellIterator.hasNext()) {
 			Cell cell = cellIterator.next();
 			if (cell.getStringCellValue().equals(headerName)) {
-				index = cell.getColumnIndex();	
+				index = cell.getColumnIndex();
 			}
 		}
 		return index;
 	}
-	
+
 	private Sheet getSheet(String sheetName) {
 		return allSheet.get(sheetName);
 	}
-	
-	
+
 	private static String ExcelPath(String filePath) {
 		if (new File(filePath).exists()) {
 			return filePath;
